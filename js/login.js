8969d72a18c9c2c891b5d18e47e4103403559414
zhangYiMin1992 <470779536@qq.com>
2017-05-02 22:14:48 +0800

    style modify

--- js/login.js
+++ js/login.js
@@ -21,17 +21,6 @@ var phoneInputCheck=function(e){
 }
 
 /**
- * 弹框
- */
-$(function(){
-    initMessage(),
-    window.alert = function(e) {
-        message.info = e,
-        showMsg(message)
-    }
-});
-
-/**
  * 侧边栏 二级菜单
  */
 
@@ -42,9 +31,9 @@ $(function(){
     })
     $(document).on("click", ".panel-overlay", function(e) {
         $.closePanel();
+        $('#leftSidebar dl dd').slideUp();
+        $('#leftSidebar dl').removeClass('on'); 
     });
-
-
 
     // 侧边栏菜单
     $('#leftSidebar .select dt').click(function(e) {
@@ -73,19 +62,17 @@ $(function(){
     //         }
     //     }
     // });
-    $('#loginPhone').get(0).onkeyup = phoneInputCheck;
+    // $('#loginPhone').get(0).onkeyup = phoneInputCheck;
         //点击登录按钮
     $('#login_menu').click(function() {
         var name = $.trim($('#loginPhone').val());
         var psw = $.trim($("#loginPwd").val());
         if (!$.YGG.regExp.cellPhone.test(name)) {          
-            maskShow('请输入正确的手机号码');
-            $('#loginPhone').focus();  
+            maskShow('请输入正确的手机号码');  
             return;      
         }
         if(psw == ''){
             maskShow('请输入密码');
-            $("#loginPwd").focus();
             return;
         }
         $.ajax({
@@ -122,7 +109,7 @@ $(function() {
         $('#register').removeClass('page-current');
     });
 
-    $('#registerPhone').get(0).onkeyup = phoneInputCheck;
+    // $('#registerPhone').get(0).onkeyup = phoneInputCheck;
 
     //短信发送
     $('#get_mobile_code').click(function() {
@@ -138,7 +125,6 @@ $(function() {
             success: function(res) {
                 if (res.isExist) { 
                     maskShow("此手机号已经注册");
-                    $('#registerPhone').focus();
                 }else{
                     $.YGG.ajax({
                         type: 'GET',
@@ -151,7 +137,6 @@ $(function() {
                             }else{
                                 maskShow(res.message);
                             }
-                            $("#mobile_code").focus();
                         }
                     });
                 }
@@ -166,32 +151,26 @@ $(function() {
         var pwdAgain=$.trim($("#pwdAgain").val());
         if (!$.YGG.regExp.cellPhone.test(mobile)) {
             maskShow('请输入正确的手机号码');
-            $('#registerPhone').focus();
             return;
         }
         if(pwd==''){
              maskShow('请输入密码');
-             $("#pwd").focus();
              return;
         }
         if(pwd.length<6 || pwd.length>16){
             maskShow('密码长度为6-16位数字字母符号组合');
-            $("#pwd").focus();
             return;
         }
         if(pwdAgain==''){
              maskShow('请再次输入密码');
-             $("#pwdAgain").focus();
              return;
         }
         if(pwd!==pwdAgain){
             maskShow('密码输入不一致');
-            $("#pwdAgain").focus();
             return;
         }
         if(mobileCode==""){
             maskShow('请输入验证码');
-            $('#mobile_code').focus();
             return;
         }
         $.YGG.ajax({
@@ -225,7 +204,289 @@ $(function() {
         });
     });
 
+    // 找回密码
+    //验证码发送
+    $('#forgot_get_mobile_code').click(function() {
+        var mobile=$.trim($('#findPhone').val());
+        if (!$.YGG.regExp.cellPhone.test(mobile)) {
+            maskShow('请输入正确的手机号码');
+            return;
+        }
+        $.YGG.ajax({
+            type: "POST",
+            dataType: "json",
+            url: "newwap/user/userExist?username=" +mobile,
+            success: function(res) {
+                if (res.isExist) {
+                    $.YGG.ajax({
+                        type: 'GET',
+                        dataType: 'json',
+                        url: "/newwap/message/sendVerificationCode?mobile=" + mobile,
+                        success: function(res) {
+                            if(res.isSuccess){
+                                maskShow("验证码已发送至手机");
+                                $('#forgot_get_mobile_code').val('已发送');
+                            }else{
+                                maskShow(res.message);
+                            }
+                        }
+                    }); 
+                }else{
+                    maskShow("此手机号没有注册");
+                }
+            }
+        });
+    });
+    // 点击找回密码按钮
+    $('#find_menu').click(function(){
+        var mobile=$.trim($('#findPhone').val());
+        var mobileCode=$.trim($('#forgot_get_mobile_code').val());
+        if (!$.YGG.regExp.cellPhone.test(mobile)) {
+            maskShow('请输入正确的手机号码');
+            return;
+        }
+        if(mobileCode==""){
+            maskShow('请输入验证码');
+            return;
+        }
+        $.YGG.ajax({
+           type:"POST" ,
+           dataType:"json",
+           url:"/newwap/message/verifyVerificationCode?verificationCode=" + mobileCode,
+           success:function(res){
+            if(res.isSuccess){
+                $.YGG.ajax({
+                    type:"POST",
+                    dataType:"json",
+                    url:"newwap/user/userFindPassword",
+                    data:{
+                        username: mobile,
+                        verificationCode: mobileCode
+                    } ,
+                    success:function(res){
+                        if(res.isSuccess){
+                            maskShow("验证成功");
+                            //跳转到重置页面
+                           // window.location.href="/newwap/usercenter/index.php";  
+                        }else{
+                            maskShow("请检查填写信息是否准确");
+                        }
+                    }
+                });
+            }
+           }
+        });
+    });
+    // 重置密码
+    $('#reset_menu').click(function(){
+        var pwd=$.trim($("#password").val());
+        var pwdAgain=$.trim($("#passwordAgain").val());
+        if(pwd.length<6 || pwd.length>16){
+           maskShow('密码长度为6-16位数字字母符号组合');
+           return; 
+        }
+        if(pwdAgain==''){
+             maskShow('请再次输入密码');
+             return;
+        }
+        if(pwd!==pwdAgain){
+            maskShow('密码输入不一致');
+            return;
+        }
+        $.YGG.ajax({
+            type:"POST",
+            dataType:"json",
+            url:"/newwap/user/userreset=" + pwdAgain, 
+            success:function(res){
+                if(res.isSuccess){
+                    maskShow("修改成功");
+                }else{
+                    maskShow("修改失败，请检查填写密码是否准确");
+                }
+            }
+        });
+    });
 
 });
+//首页轮播
+;(function($){
+    var mobileMove=function(mobileMove)
+    {
+        this.mobileMove=mobileMove;
+        this.imgList=mobileMove.find("[data-module='content']");
+        this.imgA=this.imgList.find("a");
+
+        this.nav=mobileMove.find("[data-module='nav']");
+        this.navLi=this.nav.find("li");
+
+        var first=this.imgA.first().clone(),
+               last=this.imgA.last().clone();
+        this.imgList.append(first);
+        this.imgList.prepend(last);
+        this.imgA=this.imgList.find("a");
+        this.curIndex=1;
+        this.length=this.imgA.length;
+
+        this.setting={
+
+        };
+        $.extend(this.setting,this.getSettingValue());
+        //console.log(this.CSS3({ "transform": "translate3d(" + 100 * 1 + "%, 0, 0)" }));
+        //设置页面位置关系
+        this.initPage();
+        this.bindEvent();
+    }
+    mobileMove.init=function(mobileMoves)
+    {
+        var _this_=this;
+        mobileMoves.each(function(){
+            new _this_($(this));
+        });
+    }
+    mobileMove.prototype= {
+        getSettingValue:function()
+        {
+            var settingStr=this.mobileMove.attr("setting-value");
+            if(settingStr&&""!=settingStr)
+            {
+                try{
+                    return $.parseJSON(settingStr);
+                }catch(ex){
+                    console.log("配置参数错误！"+ex);
+                    return {};
+                }
+            }else
+            {
+                return {};
+            }
+        },
+        CSS3:function()//设置浏览器兼容
+        {
+            var o = {},
+            output;
+            var prefix = ["-webkit-", "-moz-", "-ms-", "-o-"];
+            if (typeof arguments[0] == "string") {
+                o[arguments[0]] = arguments[1]
+            } else {
+                o = arguments[0]
+            }
+            for (var k in o) {
+                for (var i = 0, len = prefix.length; i < len; i++) {
+                    o[prefix[i] + k] = o[k]
+                }
+            }
+            return o;
+        },
+        loadImage: function($img, callback) {
+            if (! ($img && $img.length)) return;
+            var src = $img.attr("data-src");
+            $img.attr("src", src).removeAttr("data-src");
+            callback && ($img[0].onload = function() {
+                callback();
+                $img[0].onload = null
+            })
+        },
+        initPage:function(){
+            var that = this;
+            that.imgA.each(function(k, v) {
+                $(this).css(that.CSS3({
+                    "transform": "translate3d(" + 100 * k + "%, 0, 0)"
+                }))
+            });
+            this.setPagePos( - 100 * that.curIndex+"%",false);
+        },
+        setPagePos:function(pos,needTransition)
+        {
+            var that=this;
+            that.loadImage(that.imgA.eq(that.curIndex).prev().find("img[data-src]"));
+            that.loadImage(that.imgA.eq(that.curIndex).next().find("img[data-src]"));
+             var ani = {};
+             ani.transition = needTransition ? "all 0.4s ease": "all 0s ease";
+             ani.transform = "translate3d(" +pos+ ", 0, 0)";
+             that.imgList.css(this.CSS3(ani));
+             that.navLi.removeClass("on").eq(that.curIndex - 1).addClass("on");
+             setTimeout(function() {
+                 if (that.curIndex <= 0) {
+                     that.curIndex = that.length - 2;
+                     that.setPagePos( - 100 * that.curIndex + "%", false);
+                     return false
+                 }
+                 if (that.curIndex >= that.length - 1) {
+                     that.curIndex = 1;
+                     that.setPagePos( - 100 * that.curIndex + "%", false);
+                     return false
+                 }
+             },
+             400)
+        },
+        stop: function() {
+            var that = this;
+            window.clearInterval(that.interval)
+        },
+        play: function() {
+            var that = this;
+            that.stop();
+            that.interval = window.setInterval(function() {
+                that.curIndex++;
+                that.setPagePos( - 100 * that.curIndex + "%", true)
+            },
+            4000)
+        },
+        bindEvent: function() {
+            var that = this;
+            var $el = that.imgList;
+            var pos = {};
+            var direction = "";
+            $el[0].addEventListener("touchstart",
+            function(ev) {
+                direction = "";
+               // that.stop();
+                pos.x1 = ev.touches[0].pageX;
+                pos.y1 = ev.touches[0].pageY;//touches[0]:获取手指列表中第一个元素
+                return
+            },
+            false);
+            
+            $el[0].addEventListener("touchmove", 
+            function(ev) {
+                pos.x2 = ev.touches[0].pageX;
+                pos.y2 = ev.touches[0].pageY;
+                var x = pos.x2 - pos.x1;
+                var y = pos.y2 - pos.y1; ! direction && (direction = Math.abs(y) > Math.abs(x) ? "vertical": "horizontal");
+                if (direction == "vertical") {
+                    return
+                }
+                var width = $el.width();
+                that.setPagePos(x - that.curIndex * width + "px",false);
+                ev.preventDefault();
+                return false
+            },
+            false);    
+            $el[0].addEventListener("touchend", 
+            function(ev) {
+                pos.x2 = ev.changedTouches[0].pageX;
+                pos.y2 = ev.changedTouches[0].pageY;
+                var x = pos.x2 - pos.x1;
+                var y = pos.y2 - pos.y1;
+                var width = $el.width();
+                if (direction == "vertical" || Math.abs(x) < 30) {
+                    return
+                }
+                if (x > 0) {
+                    that.curIndex--
+                } else {
+                    that.curIndex++
+                }
+                that.setPagePos( - 100 * that.curIndex + "%", true);
+                that.play();
+                ev.preventDefault();
+                return false
+            },
+            false)
+        }
+    }
+    window["mobileMove"]=mobileMove;
+ })(jQuery)
+mobileMove.init($(".MobileMove"));
 
 
