b9f22625c436b2b8e4ae364423a02dc06b20a205
zhangYiMin1992 <470779536@qq.com>
2017-05-03 00:34:16 +0800

    修改

--- js/index.js
+++ js/index.js
@@ -1,165 +1,121 @@
-$(function() {
-    $(document).on("click", ".open-panel", function(e) {
-        var panel = $(e.target).data('panel');
-        $.openPanel(panel);
-    })
-    $(document).on("click", ".panel-overlay", function(e) {
-        $.closePanel();
-    });
+// $(function() {
+//     $(document).on("click", ".open-panel", function(e) {
+//         var panel = $(e.target).data('panel');
+//         $.openPanel(panel);
+//     })
+//     $(document).on("click", ".panel-overlay", function(e) {
+//         $.closePanel();
+//     });
 
 
 
-    // 侧边栏菜单
-    $('#leftSidebar .select dt').click(function(e) {
-        if ($(this).parent().attr('class') != 'select on') {
-            $(this).parent().addClass('on');
-        } else {
-            $(this).parent().removeClass('on');
-        }
-        var $dd = $(this).siblings();
-        $dd.slideToggle(100);
-    });
-})
-
+//     // 侧边栏菜单
+//     $('#leftSidebar .select dt').click(function(e) {
+//         if ($(this).parent().attr('class') != 'select on') {
+//             $(this).parent().addClass('on');
+//         } else {
+//             $(this).parent().removeClass('on');
+//         }
+//         var $dd = $(this).siblings();
+//         $dd.slideToggle(100);
+//     });
+// })
 //首页轮播
-;(function($){
-    var mobileMove=function(mobileMove)
-    {
-        this.mobileMove=mobileMove;
-        this.imgList=mobileMove.find("[data-module='content']");
-        this.imgA=this.imgList.find("a");
-
-        this.nav=mobileMove.find("[data-module='nav']");
-        this.navLi=this.nav.find("li");
-
-        var first=this.imgA.first().clone(),
-               last=this.imgA.last().clone();
-        this.imgList.append(first);
-        this.imgList.prepend(last);
-        this.imgA=this.imgList.find("a");
-        this.curIndex=1;
-        this.length=this.imgA.length;
-
-        this.setting={
-
-        };
-        $.extend(this.setting,this.getSettingValue());
-        //console.log(this.CSS3({ "transform": "translate3d(" + 100 * 1 + "%, 0, 0)" }));
-        //设置页面位置关系
-        this.initPage();
-        this.bindEvent();
+;(function(w, $, undefined) {
+    if (!$) {
+        return
     }
-    mobileMove.init=function(mobileMoves)
-    {
-        var _this_=this;
-        mobileMoves.each(function(){
-            new _this_($(this));
-        });
-    }
-    mobileMove.prototype= {
-        getSettingValue:function()
-        {
-            var settingStr=this.mobileMove.attr("setting-value");
-            if(settingStr&&""!=settingStr)
-            {
-                try{
-                    return $.parseJSON(settingStr);
-                }catch(ex){
-                    console.log("配置参数错误！"+ex);
-                    return {};
-                }
-            }else
-            {
-                return {};
+    var CSS3 = function() {// $(this).css( CSS3({ "transform": "translate3d(" + 100 * k + "%, 0, 0)" }) )
+        var o = {},
+        output;
+        var prefix = ["-webkit-", "-moz-", "-ms-", "-o-"];
+        if (typeof arguments[0] == "string") {
+            o[arguments[0]] = arguments[1]
+        } else {
+            o = arguments[0]
+        }
+        for (var k in o) {
+            for (var i = 0, len = prefix.length; i < len; i++) {
+                o[prefix[i] + k] = o[k]
             }
+        }
+        return o
+    };
+    var Banner = function(o) {
+        o = o || {};
+        this.o = $.extend({
+            init: function() {},
+            before: function() {},
+            moving: function() {},
+            after: function() {}
         },
-        CSS3:function()//设置浏览器兼容
-        {
-            var o = {},
-            output;
-            var prefix = ["-webkit-", "-moz-", "-ms-", "-o-"];
-            if (typeof arguments[0] == "string") {
-                o[arguments[0]] = arguments[1]
-            } else {
-                o = arguments[0]
-            }
-            for (var k in o) {
-                for (var i = 0, len = prefix.length; i < len; i++) {
-                    o[prefix[i] + k] = o[k]
-                }
-            }
-            return o;
-        },
-        loadImage: function($img, callback) {
-            if (! ($img && $img.length)) return;
-            var src = $img.attr("data-src");
-            $img.attr("src", src).removeAttr("data-src");
-            callback && ($img[0].onload = function() {
-                callback();
-                $img[0].onload = null
-            })
-        },
-        initPage:function(){
+        o);
+        this.init()
+    };
+    Banner.prototype = {
+        initData: function() {
             var that = this;
-            that.imgA.each(function(k, v) {
-                $(this).css(that.CSS3({
+            that.touchLock = false;
+            that.interval = null;
+            that.$el = that.o.$el;
+            
+            that.content = that.$el.find("[data-module=content]");
+            that.item = that.content.children();
+            var first = that.item.first().clone();
+            var last = that.item.last().clone();
+            that.content.append(first);
+            that.content.prepend(last);
+            that.item = that.content.children();
+            that.length = that.item.length;
+            that.index = 1;
+            that.nav = that.$el.find("[data-module=nav]")
+        },
+        initPage: function() {
+            var that = this;
+            that.item.each(function(k, v) {
+                $(this).css(CSS3({
                     "transform": "translate3d(" + 100 * k + "%, 0, 0)"
                 }))
             });
-            this.setPagePos( - 100 * that.curIndex+"%",false);
+            that.setPos( - 100 * that.index + "%", false)
         },
-        setPagePos:function(pos,needTransition)
-        {
-            var that=this;
-            that.loadImage(that.imgA.eq(that.curIndex).prev().find("img[data-src]"));
-            that.loadImage(that.imgA.eq(that.curIndex).next().find("img[data-src]"));
-             var ani = {};
-             ani.transition = needTransition ? "all 0.4s ease": "all 0s ease";
-             ani.transform = "translate3d(" +pos+ ", 0, 0)";
-             that.imgList.css(this.CSS3(ani));
-             that.navLi.removeClass("on").eq(that.curIndex - 1).addClass("on");
-             setTimeout(function() {
-                 if (that.curIndex <= 0) {
-                     that.curIndex = that.length - 2;
-                     that.setPagePos( - 100 * that.curIndex + "%", false);
-                     return false
-                 }
-                 if (that.curIndex >= that.length - 1) {
-                     that.curIndex = 1;
-                     that.setPagePos( - 100 * that.curIndex + "%", false);
-                     return false
-                 }
-             },
-             400)
-        },
-        stop: function() {
+        setPos: function(pos, needTransition) {
             var that = this;
-            window.clearInterval(that.interval)
-        },
-        play: function() {
-            var that = this;
-            that.stop();
-            that.interval = window.setInterval(function() {
-                that.curIndex++;
-                that.setPagePos( - 100 * that.curIndex + "%", true)
+            that.loadImage(that.item.eq(that.index).prev().find("img[data-src]"));
+            that.loadImage(that.item.eq(that.index).next().find("img[data-src]"));
+            var ani = {};
+            ani.transition = needTransition ? "all 0.4s ease": "all 0s ease";
+            ani.transform = "translate3d(" + pos + ", 0, 0)";
+            that.content.css(CSS3(ani));
+            that.nav.children().removeClass("on").eq(that.index - 1).addClass("on");
+            setTimeout(function() {
+                if (that.index <= 0) {
+                    that.index = that.length - 2;
+                    that.setPos( - 100 * that.index + "%", false);
+                    return false
+                }
+                if (that.index >= that.length - 1) {
+                    that.index = 1;
+                    that.setPos( - 100 * that.index + "%", false);
+                    return false
+                }
             },
-            4000)
+            400)
         },
         bindEvent: function() {
             var that = this;
-            var $el = that.imgList;
+            var $el = that.$el;
             var pos = {};
             var direction = "";
-            $el[0].addEventListener("touchstart",
+            $el[0].addEventListener("touchstart", 
             function(ev) {
                 direction = "";
-               // that.stop();
+                that.stop();
                 pos.x1 = ev.touches[0].pageX;
-                pos.y1 = ev.touches[0].pageY;//touches[0]:获取手指列表中第一个元素
+                pos.y1 = ev.touches[0].pageY;
                 return
             },
             false);
-            
             $el[0].addEventListener("touchmove", 
             function(ev) {
                 pos.x2 = ev.touches[0].pageX;
@@ -170,11 +126,11 @@ $(function() {
                     return
                 }
                 var width = $el.width();
-                that.setPagePos(x - that.curIndex * width + "px",false);
+                that.setPos(x - that.index * width + "px", false);
                 ev.preventDefault();
                 return false
             },
-            false);    
+            false);
             $el[0].addEventListener("touchend", 
             function(ev) {
                 pos.x2 = ev.changedTouches[0].pageX;
@@ -186,19 +142,66 @@ $(function() {
                     return
                 }
                 if (x > 0) {
-                    that.curIndex--
+                    that.index--
                 } else {
-                    that.curIndex++
+                    that.index++
                 }
-                that.setPagePos( - 100 * that.curIndex + "%", true);
+                that.setPos( - 100 * that.index + "%", true);
                 that.play();
                 ev.preventDefault();
                 return false
             },
             false)
+        },
+        play: function() {
+            var that = this;
+            that.stop();
+            that.interval = w.setInterval(function() {
+                that.index++;
+                that.setPos( - 100 * that.index + "%", true)
+            },
+            4000)
+        },
+        stop: function() {
+            var that = this;
+            w.clearInterval(that.interval)
+        },
+        loadImage: function($img, callback) {
+            if (! ($img && $img.length)) return;
+            var src = $img.attr("data-src");
+            $img.attr("src", src).removeAttr("data-src");
+            callback && ($img[0].onload = function() {
+                callback();
+                $img[0].onload = null
+            })
+        },
+        init: function() {
+            var that = this;
+            that.touchLock = false;
+            that.interval = null;
+            that.$el = that.o.$el;
+            that.content = that.$el.find("[data-module=content]");
+            that.item = that.content.children();
+            that.length = that.item.length;
+            if (that.length <= 1) return;
+            that.loadImage(that.item.last().find("img[data-src]"), 
+            function() {
+                that.initData();
+                that.initPage();
+                that.bindEvent();
+                that.play();
+                that.o.init(that)
+            })
         }
+    };
+    $.fn.Banner = function() {
+        return this.each(function() {
+            new Banner({
+                $el: $(this)
+            })
+        })
     }
-    window["mobileMove"]=mobileMove;
- })(jQuery)
-mobileMove.init($(".MobileMove"));
-
+})(window, $);
+$(function(){
+    $("#benlai_banner").Banner()
+})
