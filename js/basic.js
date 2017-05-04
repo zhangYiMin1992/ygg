f4b903d25e12f7065434181850988c6fe633ab97
Your Name <you@example.com>
2017-05-04 10:21:52 +0800

    轮播

--- js/basic.js
+++ js/basic.js
@@ -3835,6 +3835,33 @@ var CSS3 = function() {// $(this).css( CSS3({ "transform": "translate3d(" + 100 
 })(Zepto)
 
 
+/**
+ * 侧边栏 二级菜单
+ */
+
+;$(function(){
+    $(document).on("click",".open-panel",function(e){
+        var panel = $(e.target).data('panel');
+        $.openPanel(panel);
+    })
+    $(document).on("click", ".panel-overlay", function(e) {
+        $.closePanel();
+        $('#leftSidebar dl dd').slideUp();
+        $('#leftSidebar dl').removeClass('on'); 
+    });
+
+    // 侧边栏菜单
+    $('#leftSidebar .select dt').click(function(e) {
+        if ($(this).parent().attr('class') != 'select on') {
+            $(this).parent().addClass('on');
+        } else {
+            $(this).parent().removeClass('on');
+        }
+        var $dd = $(this).siblings();
+        $dd.slideToggle(100);
+    });
+})
+
 
 
 
--- js/index.js
+++ js/index.js
@@ -1,25 +1,4 @@
-// $(function() {
-//     $(document).on("click", ".open-panel", function(e) {
-//         var panel = $(e.target).data('panel');
-//         $.openPanel(panel);
-//     })
-//     $(document).on("click", ".panel-overlay", function(e) {
-//         $.closePanel();
-//     });
 
-
-
-//     // 侧边栏菜单
-//     $('#leftSidebar .select dt').click(function(e) {
-//         if ($(this).parent().attr('class') != 'select on') {
-//             $(this).parent().addClass('on');
-//         } else {
-//             $(this).parent().removeClass('on');
-//         }
-//         var $dd = $(this).siblings();
-//         $dd.slideToggle(100);
-//     });
-// })
 //首页轮播
 ;(function(w, $, undefined) {
     if (!$) {
