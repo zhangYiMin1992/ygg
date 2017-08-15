dfba2e48c0c29b518581d3c5473a3d14ff038654
simple <470779536@qq.com>
2017-08-15 18:38:44 +0800

    接口测试

--- js/index.js
+++ js/index.js
@@ -11,6 +11,33 @@ function maskShow(message) {
     }, 100);
 }
 
+//// 简单的节流函数
+function throttle(func, wait, mustRun) {
+    var timeout,
+            startTime = new Date();
+
+    return function () {
+        var context = this,
+                args = arguments,
+                curTime = new Date();
+
+        clearTimeout(timeout);
+        // 如果达到了规定的触发时间间隔，触发 handler
+        if (curTime - startTime >= mustRun) {
+            func.apply(context, args);
+            startTime = curTime;
+            // 没达到触发间隔，重新设定定时器
+        } else {
+            timeout = setTimeout(func, wait);
+        }
+    };
+}
+
+//获取发送ajax条件的位置
+function loading($el){
+    return $(window).height()+$(window).scrollTop()>$el.offset().top
+}
+
 //首页轮播
 ;(function(w, $, undefined) {
     if (!$) {
